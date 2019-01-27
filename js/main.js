81c32aa0386db78c2b037a543c4dd8d0c894b6c5
chichanes <chichanes@gmail.com>
2019-01-27 14:12:20 +0000

    no message

--- js/main.js
+++ js/main.js
@@ -58,12 +58,12 @@ function draw() {
 	// TIMER
 	
 	if (started) {
-	if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-    timer --;
-  }
-  if (timer == 0) {
-    text("GAME OVER", width/2, height*0.7);
-  }
+		if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+		timer --;
+	  }
+	  if (timer == 0) {
+		text("GAME OVER", width/2, height*0.7);
+	  }
 	}
 	
 	
